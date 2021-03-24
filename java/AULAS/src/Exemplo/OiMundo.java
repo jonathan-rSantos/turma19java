ca6f631501775ec88c6146fcc9102bbde1671f8d
Jonathan <jonathan.ribeiro28121997@gmail.com>
2021-03-24 16:45:20 -0300

    primeiro commit turma 19 agora em java

---
+++ java/AULAS/src/Exemplo/OiMundo.java
@@ -0,0 +1,12 @@
+package Exemplo;
+
+
+public class OiMundo {
+		
+		public static void main(String[] args) {
+			
+		System.out.println("Ola turma GENERATION");
+		System.out.println("JAVA É SIMPLES, MAS NÃO FACIL!!");
+		
+}
+}
