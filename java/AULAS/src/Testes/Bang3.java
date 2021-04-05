2b174716847d5b97a24d35f98a7f0aac646f4eb4
Jonathan <jonathan.ribeiro28121997@gmail.com>
2021-04-05 07:11:42 -0300

    Atualização projeto bang3

--- java/AULAS/src/Testes/Bang3.java
+++ java/AULAS/src/Testes/Bang3.java
@@ -12,22 +12,23 @@ public class Bang3 {
 		boolean ativa;
 		
 		System.out.println("BEM VINDO AO BANG-3 \nAO TIRO CERTO NA MELHOR EXCOLHA!");
+		System.out.print("DIGITE O CPF:");
+		cpf = leia.nextLong();
+		System.out.print("DIGITE A SENHA:");
+		senha = leia.nextInt();
 		System.out.println("SELECIONE A CONTA DESEJADA:");
 		System.out.println("1- CONTA CORRENTE \n2- CONTA POUPANÇA \n"
-				+ "3- CONTA ESPECIAL \n4- CONTA EMPRESA \nCONTA ESTUDANTIL \n"
+				+ "3- CONTA ESPECIAL \n4- CONTA EMPRESA \n5- CONTA ESTUDANTIL \n"
 				+ "6 - SAIR");
 		conta = leia.nextInt();
 		if (conta == 1) {
-			System.out.print("CONTA CORRENTE! \nDIGITE O CPF:");
-			cpf = leia.nextLong();
-			System.out.print("DIGITE A SENHA:");
-			senha = leia.nextInt();
+			System.out.print("CONTA CORRENTE:");
 			System.out.println("SALDO ATUAL: R$" + saldo
 					+"\nDESEJA FAZER UM MOVIMENTO?  \nDIGITE 1 PARA SIM"
 					+ " OU 2 PARA SAIR. \n1- SIM  /  2- SAIR? \n8-EMPRESTIMO  ");
 			continuar = leia.nextInt();
 			if (continuar == 1) {
-				System.out.println("1- DEBITOU OU 2- CREDITO?");
+				System.out.println("1- DEBITO OU 2- CREDITO?");
 				debito = leia.nextInt();
 				if (debito == 1) {
 					System.out.print("VALOR DO MOVIMENTO:");
@@ -37,9 +38,9 @@ public class Bang3 {
 								+ "DESEJA USAR LIMITE DE CONTA CORRENTE\nSEU LIMITE É: "+limiteContaCorrente+ "R$ \n"
 								+ "DIGITE 1 - PARA SIM \n2-PARA SAIR");
 						continuar = leia.nextInt();
-						if(movimento < limiteContaCorrente) {
+						if(movimento < limiteContaCorrente || movimento < saldo) {
 							for (int x = 1; x <=10; x++) {
-							if (continuar == 1 || limiteContaCorrente > 0 ) {
+							if (continuar == 1) {
 							System.out.print("OPERAÇÃO REALIZADA!! \n");
 							saldo += movimento;
 							limiteContaCorrente -= movimento;
@@ -49,14 +50,18 @@ public class Bang3 {
 							continuar = leia.nextInt();
 							System.out.print("\nVALOR DO MOVIMENTO:");
 							movimento = leia.nextInt();
-							} else {
-									break;
+							} if (movimento > limiteContaCorrente) {
+							System.out.println("VALOR ACIMA DO LIMITE \nOPERAÇÃO FINALIZADA!");
+									return;
 								}
 							}
 							}
 						}
 					}
 				}
+			} if (conta == 2) {
+				System.out.println("CONTA POUPANÇA"
+						+ "\nSALDO POUPANÇA:");
 			}
 		}
 	}
