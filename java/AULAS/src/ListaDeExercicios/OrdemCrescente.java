61f5e130f56d086ff5efb3adb28c021cbea53119
Jonathan <jonathan.ribeiro28121997@gmail.com>
2021-04-05 14:53:51 -0300

    Atualização de exercicios 05/04/2021

--- java/AULAS/src/ListaDeExercicios/OrdemCrescente.java
+++ java/AULAS/src/ListaDeExercicios/OrdemCrescente.java
@@ -19,33 +19,17 @@ public class OrdemCrescente {
 		System.out.println("3º numero:");	
 		numero3 = leia.nextInt();
 			
-		if (numero1 > numero2 && numero2 > numero3) {
-			System.out.println(numero1+","+ numero2 + "," + numero3);
+		if (numero1 < numero2 && numero2 < numero3) {
+			System.out.println(numero1 + ","+ numero2 + "," + numero3);
 		}
-		if (numero1 > numero2 && numero2 < numero3 && numero3 < numero1) {
-			System.out.println(numero1 + "," + numero3 + "," + numero2);
+		else if (numero1 < numero2 && numero2 > numero3) {
+			System.out.println(numero1 + ","+ numero3 + "," + numero2);
 		}
-		if (numero1 > numero2 && numero2 < numero3 && numero3 > numero1) {
-			System.out.println(numero3 + "," + numero1 + "," + numero2);
-		}
-		if (numero1 < numero2 && numero2 > numero3 && numero3 < numero1) {
-			System.out.println(numero2 + "," + numero1 + "," + numero3);
-		}
-		if (numero1 == numero2 && numero3 > numero2) {
-			System.out.println(numero3 + "," + numero1 + "," + numero2);
-		}
-		if (numero1 > numero2 && numero1 > numero3 && numero2 == numero3) {
-		System.out.println(numero1 + "," + numero2 + "," + numero3);
-		}
-		if(numero1 < numero2 && numero2 < numero3) {
+		else if (numero1 > numero2 && numero2 > numero3) {
 			System.out.println(numero3 + "," + numero2 + "," + numero1);
 		}
-		if(numero2 > numero1 && numero2 > numero3) {
+		else if (numero1 > numero2 && numero2 < numero3 ) {
 			System.out.println(numero2 + "," + numero3 + "," + numero1);
-		}
-		if (numero1 == numero2 && numero1 == numero3) {
-			System.out.println("Não há ordem crescente, numeros iram ficar assim: " + numero1 
-					+ "," + numero2 + "," + numero3);
 		}
 	}
 }
