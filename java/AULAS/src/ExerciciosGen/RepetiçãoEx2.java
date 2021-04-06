c71c695779d6ef47d3aec4e9d17c5c4216d4c619
Jonathan <jonathan.ribeiro28121997@gmail.com>
2021-04-06 17:09:56 -0300

    Exercicios GEN 06/04/2021[

--- java/AULAS/src/ExerciciosGen/RepetiçãoEx2.java
+++ java/AULAS/src/ExerciciosGen/RepetiçãoEx2.java
@@ -19,6 +19,8 @@ public class RepetiçãoEx2 {
 				contadorImpar++;
 			}
 		}
+		System.out.println("Valores pares: " + contadorPar);
+		System.out.println("Valores impares: " + contadorImpar);
 		
 		
 	}
